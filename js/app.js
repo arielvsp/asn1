10be9f171fdf2aefac10425b6b66802f055b70e1
Ariel Velikovsky <ariel@myzepa.com>
2016-12-18 19:05:40 +0200

    Add message after checking status

--- js/app.js
+++ js/app.js
@@ -10,10 +10,15 @@ function LunchCheckController($scope) {
   $scope.tooMuch  = false;
 
   $scope.checkIfTooMuch = function () {
-    if($scope.dishes.split(",").length <= 3) {
+
+  if($scope.dishes == "") {
+	$scope.showMessage = "Please enter data first";
+  } else if($scope.dishes.split(",").length <= 3) {
 		$scope.tooMuch  = false;
+		$scope.showMessage = "Please enter data first";
 	} else {
 		$scope.tooMuch  = true;
+		$scope.showMessage = "Please enter data first";
 	}
   };
 }
